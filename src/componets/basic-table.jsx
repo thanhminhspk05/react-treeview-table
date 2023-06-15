673d93ca2fdd96844af98ce6752e9692f732ef03
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-06-15 16:48:44 +0700

    finish mapping data to table

--- src/componets/basic-table.jsx
+++ src/componets/basic-table.jsx
@@ -1,9 +1,43 @@
 import { createTable, getCoreRowModel, getExpandedRowModel, useTableInstance } from '@tanstack/react-table';
-import React, { useState } from 'react';
-import EXAMPLE from '../example.json';
+import React, { useEffect, useState } from 'react';
+
+function convertDataToExpect(data) {
+  const result = {
+    method_name: 'getJobInfo(com.tmax.proobject.model.dataobject.DataObject arg0)',
+    class_name: 'com.tmax.jo.SHJO',
+    class_type: 'JOB_OBJECT',
+    subRows: [],
+  };
+  const map = new Map();
+  data.forEach((item) => {
+    const { callDepth, callee_class, callee_method, callee_type, caller_class } = item;
+    if (callDepth === 1) {
+      const obj = {
+        method_name: callee_method,
+        class_name: callee_class,
+        class_type: callee_type,
+        subRows: [],
+      };
+      result.subRows.push(obj);
+      map.set(callee_class, obj.subRows);
+    } else {
+      const subRows = map.get(caller_class);
+      if (subRows) {
+        const obj = {
+          method_name: callee_method,
+          class_name: callee_class,
+          class_type: callee_type,
+          subRows: [],
+        };
+        subRows.push(obj);
+        map.set(callee_class, obj.subRows);
+      }
+    }
+  });
+  return [result];
+}
 
 const table = createTable();
-const defaultData = [...EXAMPLE];
 
 const defaultColumns = [
   table.createDataColumn('method_name', {
@@ -49,9 +83,30 @@ const defaultColumns = [
 ];
 
 const BasicTable = () => {
-  const [data] = useState([...defaultData]);
+  const [data, setData] = useState([]);
   const [columns] = useState([...defaultColumns]);
   const [expanded, setExpanded] = useState({});
+
+  const fetchData = async () => {
+    const response = await fetch(
+      'http://101.101.209.11:14000/proobject/proobject-manager/ProminerMethodDetail?{%22dto%22:{%22declaring_class%22:%22com.tmax.jo.SHJO%22,%22method_name%22:%22getJobInfo(com.tmax.proobject.model.dataobject.DataObject%20arg0)%22,%22searchType%22:%22Forward%22}}&_=1686820402819'
+    );
+    const data = await response.json();
+    const newData = data.dto.DevMnrDto.map((row) => ({
+      callDepth: Number(row.callDepth),
+      callee_class: row.callee_class,
+      callee_method: row.callee_method,
+      callee_type: row.callee_type,
+      caller_class: row.caller_class,
+      caller_method: row.caller_method,
+    }));
+
+    setData(convertDataToExpect(newData));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const instance = useTableInstance(table, {
     data,
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    callDepth: 1,
+    callDepth: '1',
     callee_class: 'com.tmax.1',
     callee_method: 'setApplicationName.1',
     callee_type: 'DATA_OBJECT',
@@ -8,23 +8,23 @@ const data = [
     caller_method: 'Parent_method',
   },
   {
-    callDepth: 2,
+    callDepth: '2',
     callee_class: 'com.tmax.11',
     callee_method: 'setApplicationName.11',
     callee_type: 'DATA_OBJECT',
-    caller_class: 'com.tmax.11',
-    caller_method: 'setApplicationName.11',
+    caller_class: 'com.tmax.1',
+    caller_method: 'setApplicationName.1',
   },
   {
     callDepth: '2',
     callee_class: 'com.tmax.12',
     callee_method: 'setApplicationName.12',
     callee_type: 'DATA_OBJECT',
-    caller_class: 'com.tmax.11',
-    caller_method: 'setApplicationName.11',
+    caller_class: 'com.tmax.1',
+    caller_method: 'setApplicationName.1',
   },
   {
-    callDepth: 3,
+    callDepth: '3',
     callee_class: 'com.tmax.121',
     callee_method: 'setApplicationName.121',
     callee_type: 'DATA_OBJECT',
@@ -32,7 +32,7 @@ const data = [
     caller_method: 'setApplicationName.12',
   },
   {
-    callDepth: 3,
+    callDepth: '3',
     callee_class: 'com.tmax.122',
     callee_method: 'setApplicationName.122',
     callee_type: 'DATA_OBJECT',
@@ -40,7 +40,7 @@ const data = [
     caller_method: 'setApplicationName.12',
   },
   {
-    callDepth: 3,
+    callDepth: '3',
     callee_class: 'com.tmax.123',
     callee_method: 'setApplicationName.123',
     callee_type: 'DATA_OBJECT',
@@ -48,7 +48,7 @@ const data = [
     caller_method: 'setApplicationName.12',
   },
   {
-    callDepth: 4,
+    callDepth: '4',
     callee_class: 'com.tmax.1231',
     callee_method: 'setApplicationName.1231',
     callee_type: 'DATA_OBJECT',
@@ -56,7 +56,7 @@ const data = [
     caller_method: 'setApplicationName.123',
   },
   {
-    callDepth: 4,
+    callDepth: '4',
     callee_class: 'com.tmax.1232',
     callee_method: 'setApplicationName.1232',
     callee_type: 'DATA_OBJECT',
@@ -71,40 +71,47 @@ const expect = [
     class_name: 'Parent_class',
     class_type: 'JOB_OBJECT',
     sub_row: [
-      { method_name: 'setApplicationName.11', class_name: 'com.tmax.11', class_type: 'DATA_OBJECT', sub_row: [] },
       {
-        method_name: 'setApplicationName.12',
-        class_name: 'com.tmax.12',
-        class_type: 'JOB_OBJECT',
+        method_name: 'setApplicationName.1',
+        class_name: 'com.tmax.1',
+        class_type: 'DATA_OBJECT',
         sub_row: [
+          { method_name: 'setApplicationName.11', class_name: 'com.tmax.11', class_type: 'DATA_OBJECT', sub_row: [] },
           {
-            method_name: 'setApplicationName.121',
-            class_name: 'com.tmax.121',
-            class_type: 'JOB_OBJECT',
-            sub_row: [],
-          },
-          {
-            method_name: 'setApplicationName.122',
-            class_name: 'com.tmax.122',
-            class_type: 'JOB_OBJECT',
-            sub_row: [],
-          },
-          {
-            method_name: 'setApplicationName.123',
-            class_name: 'com.tmax.123',
+            method_name: 'setApplicationName.12',
+            class_name: 'com.tmax.12',
             class_type: 'JOB_OBJECT',
             sub_row: [
               {
-                method_name: 'setApplicationName.1231',
-                class_name: 'com.tmax.1231',
+                method_name: 'setApplicationName.121',
+                class_name: 'com.tmax.121',
                 class_type: 'JOB_OBJECT',
                 sub_row: [],
               },
               {
-                method_name: 'setApplicationName.1232',
-                class_name: 'com.tmax.1232',
+                method_name: 'setApplicationName.122',
+                class_name: 'com.tmax.122',
                 class_type: 'JOB_OBJECT',
                 sub_row: [],
+              },
+              {
+                method_name: 'setApplicationName.123',
+                class_name: 'com.tmax.123',
+                class_type: 'JOB_OBJECT',
+                sub_row: [
+                  {
+                    method_name: 'setApplicationName.1231',
+                    class_name: 'com.tmax.1231',
+                    class_type: 'JOB_OBJECT',
+                    sub_row: [],
+                  },
+                  {
+                    method_name: 'setApplicationName.1232',
+                    class_name: 'com.tmax.1232',
+                    class_type: 'JOB_OBJECT',
+                    sub_row: [],
+                  },
+                ],
               },
             ],
           },
@@ -113,3 +120,44 @@ const expect = [
     ],
   },
 ];
+// Cha -> 1 -> 2(2) -> 22(3) -> 223(2)
+
+function convertDataToExpect(data) {
+  const result = {
+    method_name: 'getJobInfo(com.tmax.proobject.model.dataobject.DataObject arg0)',
+    class_name: 'com.tmax.jo.SHJO',
+    class_type: '	JOB_OBJECT',
+    sub_row: [],
+  };
+  const map = new Map();
+  data.forEach((item) => {
+    const { callDepth, callee_class, callee_method, callee_type, caller_class } = item;
+    if (callDepth === 1) {
+      const obj = {
+        method_name: callee_method,
+        class_name: callee_class,
+        class_type: callee_type,
+        sub_row: [],
+      };
+      result.sub_row.push(obj);
+      map.set(callee_class, obj.sub_row);
+    } else {
+      const sub_row = map.get(caller_class);
+      if (sub_row) {
+        const obj = {
+          method_name: callee_method,
+          class_name: callee_class,
+          class_type: callee_type,
+          sub_row: [],
+        };
+        sub_row.push(obj);
+        map.set(callee_class, obj.sub_row);
+      }
+    }
+  });
+  return [result];
+}
+const expected = convertDataToExpect(data);
+console.log(expected);
+console.log(expect);
+// same result
